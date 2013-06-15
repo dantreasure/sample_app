28fee363531e8103e8e68551b7805c1c3bcf924e
Daniel Treasure <Daniel Treasure>
2013-06-14 19:41:27 -0600

    Layout up to 5.2 of tutorial

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+	let(:title) { "Ruby on Rails Tutorial Sample App |" }
+
+
   describe "Contacts page" do
 
     it "should have the h1 'Contacts'" do
@@ -12,22 +15,27 @@ describe "Static pages" do
     it "should have the title 'Contacts'" do
       visit '/static_pages/contacts'
       page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | Contacts")
+                    :text => "#{title} Contacts")
     end
 	
 	end
 
-  describe "Home page" do
+describe "Home page" do
 
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Home")
+                        :text => "Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
@@ -41,7 +49,7 @@ describe "Static pages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Help")
+                        :text => "#{title} Help")
     end
   end
 
@@ -55,7 +63,7 @@ describe "Static pages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | About Us")
+                    :text => "#{title} About Us")
 
     end
   end
