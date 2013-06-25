edcd7b41dd8ab1b938df1383cee175e3b261794d
Daniel Treasure <Daniel Treasure>
2013-06-24 18:24:55 -0600

    Finish user signup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -54,7 +54,6 @@ describe "Static pages" do
     page.should have_selector 'title', text: full_title('Contact')
     click_link "Home"
     page.should have_selector 'title', text: full_title('')
-    click_link "Sign up now!"
-    page.should have_selector 'title', text: full_title('Sign Up')
+
   end
 end
