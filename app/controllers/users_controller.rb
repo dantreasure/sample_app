8ca4aa685d8a393b72a15072ffe36bb24072afc1
Daniel Treasure <Daniel Treasure>
2013-06-28 19:10:28 -0600

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  include SessionsHelper
+
   def show
     @user = User.find(params[:id])
   end
@@ -13,6 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
